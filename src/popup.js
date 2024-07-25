5423e6521f19be30d792d4fcb09797bb2f06bb6d
Eckry <112786854+Eckry@users.noreply.github.com>
2024-07-25 10:35:16 -0600

    add local storage

--- src/popup.js
+++ src/popup.js
@@ -9,12 +9,28 @@ const $input = document.querySelector('input');
 
 let textEquation = '';
 
+window.addEventListener('load', () => {
+  katex.render('Equation...', $equation, { throwOnError: false });
+  chrome.storage.local.get('equation', (items) => {
+    if (items.equation) {
+      textEquation = items.equation;
+      $input.value = textEquation;
+      katex.render(textEquation, $equation, { throwOnError: false });
+    }
+  });
+});
+
 function copy() {
   navigator.clipboard.writeText(textEquation);
 }
 
 $input.addEventListener('input', (e) => {
   textEquation = e.target.value;
+  if (e.target.value === '') {
+    katex.render('Equation...', $equation, { throwOnError: false });
+    return;
+  }
+  chrome.storage.local.set({ equation: textEquation });
   katex.render(textEquation, $equation, { throwOnError: false });
 });
 
