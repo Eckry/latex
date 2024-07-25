c9d94b07576862b9bedd2be39db2664b21b3405e
Eckry <112786854+Eckry@users.noreply.github.com>
2024-07-25 10:20:28 -0600

    add copy button to the equation itself

--- src/popup.js
+++ src/popup.js
@@ -9,11 +9,14 @@ const $input = document.querySelector('input');
 
 let textEquation = '';
 
+function copy() {
+  navigator.clipboard.writeText(textEquation);
+}
+
 $input.addEventListener('input', (e) => {
   textEquation = e.target.value;
   katex.render(textEquation, $equation, { throwOnError: false });
 });
 
-$copy.addEventListener('click', () => {
-  navigator.clipboard.writeText(textEquation);
-});
+$copy.addEventListener('click', copy);
+$equation.addEventListener('click', copy);
