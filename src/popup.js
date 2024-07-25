b315dc234261a6b3f89981dc3a73f49b12aae051
Eckry <112786854+Eckry@users.noreply.github.com>
2024-07-24 20:46:52 -0600

    add copy functionality

--- src/popup.js
+++ src/popup.js
@@ -3,9 +3,17 @@
 import './popup.css';
 import katex from 'katex';
 
-const $equation = document.querySelector('p');
+const $equation = document.querySelector('.equation');
+const $copy = document.querySelector('.copy');
 const $input = document.querySelector('input');
 
+let textEquation = '';
+
 $input.addEventListener('input', (e) => {
-  katex.render(e.target.value, $equation, { throwOnError: false });
+  textEquation = e.target.value;
+  katex.render(textEquation, $equation, { throwOnError: false });
+});
+
+$copy.addEventListener('click', () => {
+  navigator.clipboard.writeText(textEquation);
 });
