0bd36bfa7b5b62ac6b0be3f26262ad00cdf10f5e
Eckry <112786854+Eckry@users.noreply.github.com>
2024-07-24 19:52:11 -0600

    add event listener to input

--- src/popup.js
+++ src/popup.js
@@ -3,6 +3,9 @@
 import './popup.css';
 import katex from 'katex';
 
-const h1 = document.querySelector('h1');
-h1.textContent = '';
-katex.render('c = \\pm\\sqrt{a^2 + b^2}', h1, { throwOnError: false });
+const $equation = document.querySelector('p');
+const $input = document.querySelector('input');
+
+$input.addEventListener('input', (e) => {
+  katex.render(e.target.value, $equation, { throwOnError: false });
+});
