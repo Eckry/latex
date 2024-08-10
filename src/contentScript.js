7027c251b9bb293461c7e80d82baa0b9a7cd46e0
Eckry <112786854+Eckry@users.noreply.github.com>
2024-08-09 20:11:34 -0600

    change width and height of container

--- src/contentScript.js
+++ src/contentScript.js
@@ -223,8 +223,7 @@ function screenshot() {
     img.src = screenshot;
 
     img.onload = () => {
-      const { left, top, width, height } =
-        $equationContainer.getBoundingClientRect();
+      const { left, top, width, height } = $equation.getBoundingClientRect();
       const dpr = 1.25;
       // Desired dimensions for the cropped area (e.g., 100x100 pixels)
       const cropWidth = width * dpr;
