e078809d106625ca62be5dfcb65bdc755ca39b3e
Eckry <112786854+Eckry@users.noreply.github.com>
2024-08-10 18:00:51 -0600

    remove unused code

--- src/popup.js
+++ src/popup.js
@@ -3,16 +3,12 @@ import './popup.css';
 
 const $checkBox = document.querySelector('.checkboxLATEX');
 
-$checkBox.addEventListener('click', () => {
-  (async () => {
-    const [tab] = await chrome.tabs.query({
-      active: true,
-      lastFocusedWindow: true,
-    });
-    const response = await chrome.tabs.sendMessage(tab.id, {
-      greeting: 'hellos',
-    });
-    // do something with response here, not outside the function
-    console.log(response);
-  })();
+$checkBox.addEventListener('click', async () => {
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
+
+  //Send message to activate real popup
+  await chrome.tabs.sendMessage(tab.id, {});
 });
