ed2da0988df4de897d8564c8be33e8ff4b10883c
Eckry <112786854+Eckry@users.noreply.github.com>
2024-11-01 21:38:47 -0600

    remove button to open window

---
+++ src/background.js
@@ -0,0 +1,5 @@
+chrome.action.onClicked.addListener(async (tab) => {
+  // Send a message to the content script in the active tab
+  chrome.tabs.sendMessage(tab.id, { action: 'openWindow' });
+  console.log('xd');
+});
